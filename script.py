21898ac271cb56bf72d8f05e1bbb729b3f17e525
Claire Zhao <clairezhaolj@gmail.com>
2025-02-23 22:55:11 -0500

    Update script.py

--- script.py
+++ script.py
@@ -15,18 +15,18 @@ import loguru
 
 def scrape_data_point():
     """
-    Scrapes the main headline from The Daily Pennsylvanian home page.
+    Scrapes the main opinion article from The Daily Pennsylvanian opinion page.
 
     Returns:
         str: The headline text if found, otherwise an empty string.
     """
-    req = requests.get("https://www.thedp.com")
+    req = requests.get("https://www.thedp.com/section/opinion")
     loguru.logger.info(f"Request URL: {req.url}")
     loguru.logger.info(f"Request status code: {req.status_code}")
 
     if req.ok:
         soup = bs4.BeautifulSoup(req.text, "html.parser")
-        target_element = soup.find("a", class_="frontpage-link")
+        target_element = soup.find("a", class_="standard-link")
         data_point = "" if target_element is None else target_element.text
         loguru.logger.info(f"Data point: {data_point}")
         return data_point
